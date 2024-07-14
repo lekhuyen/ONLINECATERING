22b89d30146413bfed984ea16153f46912ff1616
Pearlicia <suonghbts2302004@fpt.edu.vn>
2024-07-14 12:46:12 +0700

    INFORMATION - Repositories

---
+++ INFORMATION/Repositories/IContactRepositories.cs
@@ -0,0 +1,14 @@
+﻿using INFORMATIONAPI.Models;
+
+namespace INFORMATIONAPI.Repositories
+{
+    public interface IContactRepositories
+    {
+        Task<IEnumerable<Contact>> GetAllContacts();
+        Task<Contact> GetContactById(string id);
+        Task CreateContact(Contact contact);
+        Task<bool> UpdateContact(string id, Contact contact);
+        Task<bool> DeleteContact(string id);
+        Task<bool> RespondToMessage(string id, string response);
+    }
+}
---
+++ INFORMATION/Repositories/INewsRepositories.cs
@@ -0,0 +1,13 @@
+﻿using INFORMATIONAPI.Models;
+
+namespace INFORMATIONAPI.Repositories
+{
+    public interface INewsRepositories
+    {
+        Task<IEnumerable<News>> GetAllAsync();
+        Task<News> GetByIdAsync(string id);
+        Task CreateAsync(News news, IFormFile? imageFile);
+        Task<bool> UpdateAsync(string id, News news, IFormFile? imageFile);
+        Task<bool> DeleteAsync(string id);
+    }
+}
