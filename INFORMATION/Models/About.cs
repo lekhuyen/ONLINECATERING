86ad2e9cb1e5f5bbd94e269c615c9f3380775dd1
Pearlicia <suonghbts2302004@fpt.edu.vn>
2024-07-14 12:43:05 +0700

    7/14 push INFORMATION - folder Models

---
+++ INFORMATION/Models/About.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace INFORMATIONAPI.Models
+{
+    public class About
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+
+        [Required(ErrorMessage = "Content is required")]
+        public string Content { get; set; }
+
+        [BsonIgnoreIfNull]
+        public string? ImagePath { get; set; }
+    }
+}
