a3c013a13da7443a3a6d0054ebe7cfb9bfd1173f
PhamDuyHau <phamduyhau2000@gmail.com>
2024-07-24 22:00:11 +0700

    ADd this

---
+++ RESTAURANT/Models/OrderDish.cs
@@ -0,0 +1,15 @@
+﻿namespace RESTAURANT.API.Models
+{
+    public class OrderDish
+    {
+        public int Id { get; set; }
+
+        public int DishId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public Order? Order { get; set; }
+
+        public Dish? Dish { get; set; }
+    }
+}
