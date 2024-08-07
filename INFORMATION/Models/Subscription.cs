ea239012d227888fdac3622a2458024f327d3149
PhamDuyHau <phamduyhau2000@gmail.com>
2024-08-07 17:52:05 +0700

    up subscription

---
+++ INFORMATION/Models/Subscription.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace INFORMATION.API.Models
+{
+    public class Subscription
+    {
+        public ObjectId Id { get; set; }
+        public string Email { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
